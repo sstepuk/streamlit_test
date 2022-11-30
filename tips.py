e397f44f0585762dd98265e9a37077037b3c0da7
Semyon <118127914+sstepuk@users.noreply.github.com>
2022-11-30 22:12:51 +0300

    Update tips.py

--- tips.py
+++ tips.py
@@ -1,7 +1,7 @@
 #import yfinance as yf
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import plotly.express as px
 st.write("""
 ## Чек и чаевые
